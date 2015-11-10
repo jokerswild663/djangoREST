35392dda908c0d404d9113630a469c81f05196f9
Jeff Wyckoff <jokerswild663@gmail.com>
2015-11-09 23:21:02 -0800

    creating models for roster module

--- REST/roster/models.py
+++ REST/roster/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name_text = models.CharField(max_length=200)
+    start_date = models.DateTimeField('date hired')
+
+class Team(models.Model):
+    name_text = models.CharField(max_length=200)
+    teammate = models.ForeignKey(Employee) 
