dcdf33711db450b0cb9a552eec36992a30b33c3e
Jeff Wyckoff <jokerswild663@gmail.com>
2015-11-11 23:24:37 -0800

    returning string value for model objects

--- REST/roster/models.py
+++ REST/roster/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+import datetime
+from django.utils import timezone
 # Create your models here.
-class Employee(models.Model):
-    name_text = models.CharField(max_length=200)
-    start_date = models.DateTimeField('date hired')
 
 class Team(models.Model):
     name_text = models.CharField(max_length=200)
-    teammate = models.ForeignKey(Employee) 
+    creation_date = models.DateTimeField('date created')
+
+    def __string__(self):
+        return self.name_text
+
+class Employee(models.Model):
+    name_text = models.CharField(max_length=200)
+    start_date = models.DateTimeField('date hired')
+    team = models.ForeignKey(Team)
+
+    def __string__(self):
+        return self.name_text
